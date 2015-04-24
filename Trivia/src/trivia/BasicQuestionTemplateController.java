fb415f34fc196e0329fd0d8c6fc66315a30fc5fc
Shoshana <Shoshana@wireless-193-183.uchicago.edu>
2015-04-24 13:54:26 -0500

    A new branch for us to debug code which has no obvious error but is not working.

--- Trivia/src/trivia/BasicQuestionTemplateController.java
+++ Trivia/src/trivia/BasicQuestionTemplateController.java
@@ -24,15 +24,11 @@ public class BasicQuestionTemplateController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    Alert alert = new Alert(AlertType.CONFIRMATION);
-    alert.setTitle("How did you do?");
-    alert.setHeaderText("Correct!");
-    alert.setContentText("Good job! You should definitely continue.");
-    ButtonType buttonTypeNext = new ButtonType("Next Question");
-    ButtonType buttonTypeCancel = new ButtonType("Cancel", 
-    ButtonData.CANCEL_CLOSE);
-    alert.getButtonTypes().setAll(buttonTypeNext, buttonTypeCancel);
+    }
     
+    public void test() {
+        
+
+    }
     
 }
