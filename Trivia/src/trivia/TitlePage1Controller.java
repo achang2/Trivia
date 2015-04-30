77fe7ef3eaf779805527326fdfec4ed3bf2ffd4c
juliagately <juliagately@10.150.98.26>
2015-04-30 15:19:16 -0500

    my beautiful branch now has flawed dialog buttons

--- Trivia/src/trivia/TitlePage1Controller.java
+++ Trivia/src/trivia/TitlePage1Controller.java
@@ -7,7 +7,13 @@ package trivia;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -15,13 +21,21 @@ import javafx.fxml.Initializable;
  * @author Arthur
  */
 public class TitlePage1Controller implements Initializable {
-
+    @FXML
+    /**private void loginAction(ActionEvent event) throws Exception {
+        Stage stage = (Stage) root.getScene().getWindow();
+        System.out.println("You tried to login...");
+        Parent login = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Scene loginScene = new Scene(login);
+        stage.setScene(loginScene);
+    }
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    
     
 }
