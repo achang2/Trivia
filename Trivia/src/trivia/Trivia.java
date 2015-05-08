715bbef16f8a47ee42f3f082ec8744f2a6c76c46
Julia Gately <gatelyjulia@yahoo.com>
2015-05-08 13:20:52 -0500

    lots of changes... I tried to make everything work even if it meant changing some methods that were causing problems to comments.

--- Trivia/src/trivia/Trivia.java
+++ Trivia/src/trivia/Trivia.java
@@ -7,11 +7,10 @@ package trivia;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.Parent;
+//import javafx.fxml.FXMLLoader;
+//import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -19,6 +18,15 @@ import javafx.stage.Stage;
  * @author Arthur
  */
 public class Trivia extends Application {
+    
+     public static final String Welcome_Page = "Welcome Page"; 
+     public static final String Welcome_Page_FXML = "WelcomePage.fxml"; 
+     public static final String Title_Page = "Title Page"; 
+     public static final String Title_Page_FXML = "TitlePage.fxml"; 
+     public static final String Home_Page = "Home Page"; 
+     public static final String Home_Page_FXML = "HomePage.fxml"; 
+     public static final String ROULETTE_SCREEN = "roulette"; 
+     public static final String ROULETTE_SCREEN_FXML = "roulette.fxml"; 
     
     @Override
     public void start(Stage stage) throws Exception {
@@ -28,6 +36,24 @@ public class Trivia extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    /*public void start(Stage primaryStage) { 
+
+       ScreensController mainContainer = new ScreensController(); 
+         boolean loadScreen = mainContainer.loadScreen(Trivia.Welcome_Page, 
+                 Trivia.Welcome_Page_FXML); 
+       mainContainer.loadScreen(Trivia.Title_Page, 
+                            Trivia.Title_Page_FXML); 
+       mainContainer.loadScreen(Trivia.Home_Page, 
+                            Trivia.Home_Page_FXML); 
+
+       mainContainer.setScreen(Trivia.Welcome_Page); 
+
+       Group root = new Group(); 
+       root.getChildren().addAll(mainContainer); 
+       Scene scene = new Scene(root); 
+       primaryStage.setScene(scene); 
+       primaryStage.show();
+    }*/
 
     /**
      * @param args the command line arguments
